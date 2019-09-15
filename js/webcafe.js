a984225d0a6f435db1ce8bd01ba23fad2f089ecc
eunbiviakwon <eunbiviakwon@gmail.com>
2019-09-16 07:45:01 +0900

    웹카페 새소식 과제

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,10 +1,9 @@
+  
 var section = $('.board section');
 var tab = $('.tab');
 
 tab.on('click', function(e){
-    e.preventDefault();
-    if(OverflowEvent.type ==='keyup' && e.keyCode ===13) e.type === 'click'{
-    section.removeClass('tab-act');
-    $(this).parent().parent().addClass('tab-act');
-
-}}
+  e.preventDefault();
+  section.removeClass('tab-act');
+  $(this).parent().parent().addClass('tab-act');
+})
